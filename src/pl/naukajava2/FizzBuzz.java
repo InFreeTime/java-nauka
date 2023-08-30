d9b5071e3199dec42a3bf1b215646e21ac53e801
dakorta <interfud@wp.pl>
2023-08-30 14:28:24 +0200

    Aplikacja FizBuzz

---
+++ src/pl/naukajava2/FizzBuzz.java
@@ -0,0 +1,18 @@
+package pl.naukajava2;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i=1; i < 101; i++){
+            if (i %15 ==0){
+                System.out.println("FizzBuzz");
+            }else if (i % 3 == 0){
+                System.out.println("Fizz");
+            }else if (i % 5 == 0){
+                System.out.println("Buzz");
+            }else
+            System.out.println(i);
+        }
+    }
+}
